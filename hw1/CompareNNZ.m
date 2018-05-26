f390427e6f5ec7699c8e401052557c517fbe37cf
Disiok <simonsdsuo@gmail.com>
2018-05-26 13:44:59 -0400

    Add disp A and mesh

--- hw1/CompareNNZ.m
+++ hw1/CompareNNZ.m
@@ -10,16 +10,22 @@ for i = 1:4
     [L, U] = lu(A);
     n_nonzeros = nnz(L);
     disp(n_nonzeros);
+    n_nonzeros = nnz(A);
+    disp(n_nonzeros);
 
     ordering_method = 'rcm';
     [A, b] = ReorderLap2D(m, ordering_method);
     [L, U] = lu(A);
     n_nonzeros = nnz(L);
     disp(n_nonzeros);
+    n_nonzeros = nnz(A);
+    disp(n_nonzeros);
 
     ordering_method = 'min';
     [A, b] = ReorderLap2D(m, ordering_method);
     [L, U] = lu(A);
     n_nonzeros = nnz(L);
+    disp(n_nonzeros);
+    n_nonzeros = nnz(A);
     disp(n_nonzeros);
 end 
